d38aec7d212aea9df9e7200b6fc725769ec20c64
kanishka-shrivastava <shrivastavakanishka06@gmail.com>
2026-05-12 11:54:50 +0530

    Add sample Python code demonstrating basic features

---
+++ samplecode.py
@@ -0,0 +1,18 @@
+print("Python is working successfully!")
+
+a = 10
+b = 20
+sum_result = a + b
+
+print("Sum:", sum_result)
+
+for i in range(1, 6):
+    print("Count:", i)
+
+name = "Kanishka"
+print(f"Hello, {name}!")
+
+def square(num):
+    return num * num
+
+print("Square of 5 is:", square(5))
